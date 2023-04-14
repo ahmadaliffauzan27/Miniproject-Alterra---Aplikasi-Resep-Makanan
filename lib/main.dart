f67dbbde11c9fe87f92146c3fddc5ed7db8058af
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-04-14 14:10:24 +0700

    lock orientasi in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:resep_makanan/view/home_page.dart';
 import 'package:resep_makanan/view/register_page.dart';
 import 'package:resep_makanan/view/splash_screen.dart';
 import 'package:resep_makanan/view_model/db_manager.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // menambahkan kode berikut untuk mengunci orientasi tampilan
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return ChangeNotifierProvider(
       create: (context) => DbManager(),
       child: MaterialApp(
